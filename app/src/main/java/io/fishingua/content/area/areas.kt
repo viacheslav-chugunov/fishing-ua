05b9eb0ba6f3c5e9f855cb1ea193fe77a8e61b1f
viacheslav-chugunov <68062984+viacheslav-chugunov@users.noreply.github.com>
2020-08-01 17:50:49 +0300

    Ostapovka area added

--- app/src/main/java/io/fishingua/content/area/areas.kt
+++ app/src/main/java/io/fishingua/content/area/areas.kt
@@ -4,9 +4,11 @@ import io.fishingua.R.string
 import io.fishingua.R.drawable
 import io.fishingua.content.fish.*
 
-object Kozintsa : Area(string.kozintsa_name, drawable.kozintsa, GoogleMapPosition(50.5139602,30.0381162),
+object Kozintsa : Area(string.kozintsa_name, drawable.kozintsa, GoogleMapPosition(50.5139602, 30.0381162),
     Pike, Crucian, Carp, Bream, Roach, Zander)
-object KievReservoir : Area(string.Kiev_reservoir_name, drawable.kiev_reservoir, GoogleMapPosition(50.9319853,29.8549355),
+object KievReservoir : Area(string.Kiev_reservoir_name, drawable.kiev_reservoir, GoogleMapPosition(50.9319853, 29.8549355),
     Catfish, Zander, Asp, Bream, Crucian, Tench, Perch)
-object AmateurPond : Area(string.amateur_pond_name, drawable.amateur_pond, GoogleMapPosition(49.2640134,31.4209105),
+object AmateurPond : Area(string.amateur_pond_name, drawable.amateur_pond, GoogleMapPosition(49.2640134, 31.4209105),
     Carp, SilverCarp, Amur, Crucian, Roach, Tench, Pike, Perch, Ruff)
+object Ostapovka : Area(string.ostapovka_name, drawable.ostapovka, GoogleMapPosition(50.2314392, 33.6369601),
+    Amur, Catfish, SilverCarp, Crucian, Carp, Perch, Pike, Roach)
