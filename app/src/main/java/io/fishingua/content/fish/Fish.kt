3a35ab6634334ba8ed8c237c09606b4742d77397
viacheslav-chugunov <68062984+viacheslav-chugunov@users.noreply.github.com>
2020-08-13 01:26:33 +0300

    better fish extending

--- app/src/main/java/io/fishingua/content/fish/Fish.kt
+++ app/src/main/java/io/fishingua/content/fish/Fish.kt
@@ -4,16 +4,21 @@ import io.fishingua.structure.activity.RESOURCES
 import io.fishingua.content.bait.Bait
 import java.lang.IllegalArgumentException
 
+private val FISHES = mutableSetOf<Fish>()
+
 open class Fish(nameResId: Int, drawableResId: Int, vararg baits: Bait) {
     val baits = baits.toSet()
     val name = RESOURCES.getString(nameResId)
     val drawableId = drawableResId
 
+    init { initFish() }
+
+    private fun initFish() = FISHES.add(this)
+
     override fun toString(): String = name
 
     companion object {
-        fun getFishes() = arrayOf(Pike, Grayling, Podust, Crucian, Bream, Roach, Zander, Carp,
-            Catfish, Asp, Tench, Perch, SilverCarp, Amur, Ruff)
+        fun getFishes() = FISHES.toTypedArray()
 
         fun getFish(name: String) : Fish {
             for (fish in getFishes()) {
